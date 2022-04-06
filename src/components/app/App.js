b289ee562e1cbe7e25a0212cba50bb9ae37f1622
Aleksandr <aaardyshev@yandex.ru>
2022-04-06 16:16:59 +0300

    React lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,17 @@
 
+import { lazy, Suspense} from 'react/cjs/react.production.min';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+// import {MainPage, ComicsPage, SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
 
 
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(()=> import('../pages/MainPage'));
+const ComicsPage = lazy(()=> import('../pages/ComicsPage'));
+const SingleComicPage = lazy(()=> import('../pages/SingleComicPage'));
 
 
 const App = () =>  {
@@ -17,6 +23,7 @@ const App = () =>  {
                 <div className="app">
                 <AppHeader/>
                 <main>
+                <Suspense fallback={<Spinner/>}>
                 <Switch>
                     <Route exact path="/">
                         <MainPage/>
@@ -31,6 +38,7 @@ const App = () =>  {
                         <Page404/>
                     </Route>
                 </Switch>
+                </Suspense>
                    
                 </main>
             </div>
